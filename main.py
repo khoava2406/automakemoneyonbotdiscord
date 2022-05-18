3631356c49b9a0c21d6fb786cdbf5e91543d0bdb
Matthew A Rogers <102123822+khoava2406@users.noreply.github.com>
2022-05-18 19:38:18 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+
+import pyautogui as auto
+import time
+import random
+import datetime
+
+count = 1
+
+time.sleep(1)
+while True:
+    n = random.randrange(99999999999999999999999)
+
+    auto.write('awork')
+    auto.press('enter')
+    auto.write('adep all')
+    auto.press('enter')
+    time.sleep(30)
+    if n == 1:
+        if x == 1:
+            auto.write('awork')
+            auto.press('enter')
+            auto.write('adep all')
+            auto.press('enter')
+
+    print(count)
+    count = count + 1
+    datetime.datetime.now()
+    datetime.datetime(2009, 1, 6, 15, 8, 24, 78915)
+    print(datetime.datetime.now())
+    time.sleep
